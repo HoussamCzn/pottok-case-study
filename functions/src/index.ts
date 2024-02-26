dc7c416a72390e0d5642e21b3a99441be8e87bd6
HoussamCzn <10AUUUGH2+HoussamCzn@users.noreply.github.com>
2024-02-26 20:01:22 +0100

    feat: create matching function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,54 @@
+import * as admin from "firebase-admin";
+import {Offer, Rider} from "./models";
+import {distance as calculateDistance} from "./distance";
+import {https, logger} from "firebase-functions/v2";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+// TODO: Move this elsewhere
+interface OfferOptions {
+  riderId: string;
+  maxDistance?: number;
+  isAuthorizedToCompete?: boolean;
+  horseBreed?: string;
+  maxPrice?: number;
+  minDuration?: number;
+  maxWalkingDistance?: number;
+}
+
 /**
- * Import function triggers from their respective submodules:
+ * This function is called when a rider is looking for offers that match their
+ * preferences including experience, location, and additional filters like
+ * competition authorization, horse breed, price, and distance constraints.
  *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * @param {OfferOptions} params Parameters including riderId, maxDistance,
+ * and optional filters.
+ * @return {Promise<Array<Offer>>} A promise that will be resolved with an
+ * array of offers that match the rider's preferences.
  */
+async function matchingOffers(params: OfferOptions): Promise<Array<Offer>> {
+  const rider = (await db.collection("riders").doc(params.riderId).get()).data() as Rider;
+  const offers = await db.collection("offers").get();
+  const matched: Array<Offer> = [];
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+  offers.forEach((offerDoc) => {
+    const offerData = offerDoc.data() as Offer;
+    const offerDistance = calculateDistance(rider.location, offerData.location);
+    // TODO: Clean up this logic to make it more readable
+    const matchesCriteria = offerData.horse.requiredExperience <= rider.experience &&
+                                (params.maxDistance === undefined || offerDistance <= params.maxDistance) &&
+                                (params.isAuthorizedToCompete === undefined || offerData.horse.isAuthorizedToCompete === params.isAuthorizedToCompete) &&
+                                (params.horseBreed === undefined || offerData.horse.breed === params.horseBreed) &&
+                                (params.maxPrice === undefined || offerData.price <= params.maxPrice) &&
+                                (params.minDuration === undefined || offerData.terms.minDuration >= params.minDuration) &&
+                                (params.maxWalkingDistance === undefined || offerData.terms.maxDistance <= params.maxWalkingDistance);
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+    if (matchesCriteria) {
+      matched.push(offerData);
+    }
+  });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  return matched;
+}
+
